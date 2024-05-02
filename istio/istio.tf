14f8e291a7f97f3f082eedcd6302915361b0aa5e
kanika-raga-ai <161692952+kanika-raga-ai@users.noreply.github.com>
2024-05-02 19:16:37 +0530

    Update istio.tf

--- istio/istio.tf
+++ istio/istio.tf
@@ -2,11 +2,14 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-#istio
 resource "kubernetes_manifest" "knative_istio" {
-  manifest = file("/knative-istio/istio.yaml")
+  manifest = file("knative-istio/istio.yaml")  
+  # Specify the namespace for Istio resources
+  namespace = "istio-system"
 }
 
 resource "kubernetes_manifest" "knative_istio_net" {
-  manifest = file("/knative-istio/net-istio.yaml")
+  manifest = file("knative-istio/net-istio.yaml")  
+  # Specify the namespace for Istio resources
+  namespace = "istio-system"
 }
