9aa54e100c2cb48dd6a1794e9bae1e5d3dbaa6b9
Tarun Arora <tarun.arora.030402@gmail.com>
2024-05-20 15:22:25 +0530

    Edit es_automation script, use variables passed from userdata script
    Signed-off-by: Tarun Arora <tarun.arora.030402@gmail.com>

--- deployment-scripts/elk_automation/es_automation.sh
+++ deployment-scripts/elk_automation/es_automation.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-# Variables
-ELASTICSEARCH_URL="http://localhost:9200"
-KIBANA_URL="http://localhost:5601"
-KIBANA_USERNAME="elastic"
-KIBANA_PASSWORD="<TODO: Add Password>"
-SNAPSHOT_EXPIRY_LIMIT="30d"
-ENV_NAME="prod"
-CUSTOMER_NAME="raga"
-S3_BUCKET_NAME="test"
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 NODE1_IP KIBANA_CREDENTIALS CUSTOMER_ENV_PREFIX"
+    exit 1
+fi
 
-NDJSON_FILE="saved_objects_data_views.ndjson"
-ILM_POLICIES_FILE="ilm_policies.json"
-INDEX_TEMPLATES_FILE="index_templates.json"
-REPO_NAME="elasticsearch-snapshots"
+# Assign the arguments to variables
+NODE1_IP=$1
+KIBANA_CREDENTIALS=$2
+CUSTOMER_ENV_PREFIX=$3
+
+# Variables
+ELASTICSEARCH_URL="http://$NODE1_IP:9200"
+KIBANA_URL="http://$NODE1_IP:5601"
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+NDJSON_FILE="$SCRIPT_DIR/saved_objects_data_views.ndjson"
+ILM_POLICIES_FILE="$SCRIPT_DIR/ilm_policies.json"
+INDEX_TEMPLATES_FILE="$SCRIPT_DIR/index_templates.json"
+S3_BUCKET_NAME="$CUSTOMER_ENV_PREFIX-elasticsearch-snapshots"
+REPO_NAME="$CUSTOMER_ENV_PREFIX-elasticsearch-snapshots"
+POLICY_NAME="$CUSTOMER_ENV_PREFIX-snapshot-policy"
+SNAPSHOT_EXPIRY_LIMIT="30d"
 
 # Function to display error message and exit
 exit_with_error() {
@@ -24,7 +33,7 @@ exit_with_error() {
 # Function to authenticate and get the Kibana session cookie
 authenticate() {
   echo "Authenticating to Kibana..."
-  response=$(curl -s -u "$KIBANA_USERNAME:$KIBANA_PASSWORD" "$KIBANA_URL/api/status")
+  response=$(curl -s -u $KIBANA_CREDENTIALS "$KIBANA_URL/api/status")
   if [ $? -ne 0 ]; then
     exit_with_error "Failed to authenticate. Check Kibana URL and credentials."
   fi
@@ -34,7 +43,7 @@ authenticate() {
 # Function to import objects using Kibana Import API
 import_objects() {
   echo "Importing objects from $NDJSON_FILE..."
-  response=$(curl -s -u "$KIBANA_USERNAME:$KIBANA_PASSWORD" -X POST -H "kbn-xsrf: true" -F file=@$NDJSON_FILE "$KIBANA_URL/api/saved_objects/_import?createNewCopies=true")
+  response=$(curl -s -u $KIBANA_CREDENTIALS -X POST -H "kbn-xsrf: true" -F file=@$NDJSON_FILE "$KIBANA_URL/api/saved_objects/_import?createNewCopies=true")
   if [ $? -ne 0 ]; then
     exit_with_error "Failed to import objects. Check JSON file and Kibana connection."
   fi
@@ -48,10 +57,10 @@ create_ilm_policy() {
   local policy_name="$1"
   local policy_body="$2"
 
-  local full_policy_name="$ENV_NAME-$policy_name"
+  local full_policy_name="$CUSTOMER_ENV_PREFIX-$policy_name"
 
   echo "Creating ILM policy '$full_policy_name'..."
-  response=$(curl -s -u "$KIBANA_USERNAME:$KIBANA_PASSWORD" -X PUT -H "Content-Type: application/json" -d "$policy_body" "$ELASTICSEARCH_URL/_ilm/policy/$full_policy_name")
+  response=$(curl -s -u $KIBANA_CREDENTIALS -X PUT -H "Content-Type: application/json" -d "$policy_body" "$ELASTICSEARCH_URL/_ilm/policy/$full_policy_name")
   if ! echo "$response" | grep -q '"acknowledged":true'; then
     exit_with_error "Failed to create ILM policy '$full_policy_name'. Response: $response"
   fi
@@ -69,11 +78,11 @@ create_index_template() {
   local template_name="$1"
   local template_body="$2"
 
-  local full_template_name="$ENV_NAME-$template_name"
-  template_body=$(echo $template_body | jq --arg ENV_NAME "$ENV_NAME" '.template.settings.index.lifecycle.name |= ($ENV_NAME + "-" + .)')
+  local full_template_name="$CUSTOMER_ENV_PREFIX-$template_name"
+  template_body=$(echo $template_body | jq --arg CUSTOMER_ENV_PREFIX "$CUSTOMER_ENV_PREFIX" '.template.settings.index.lifecycle.name |= ($CUSTOMER_ENV_PREFIX + "-" + .)')
 
   echo "Creating Index Template '$full_template_name'..."
-  response=$(curl -s -u "$KIBANA_USERNAME:$KIBANA_PASSWORD" -X PUT -H "Content-Type: application/json" -d "$template_body" "$ELASTICSEARCH_URL/_index_template/$full_template_name")
+  response=$(curl -s -u $KIBANA_CREDENTIALS -X PUT -H "Content-Type: application/json" -d "$template_body" "$ELASTICSEARCH_URL/_index_template/$full_template_name")
   if ! echo "$response" | grep -q '"acknowledged":true'; then
     exit_with_error "Failed to create ILM policy '$full_template_name'. Response: $response"
   fi
@@ -88,7 +97,6 @@ create_index_templates() {
 }
 
 create_snapshot_repository() {
-  local repository_name="$CUSTOMER_NAME-$ENV_NAME-$REPO_NAME"
   JSON_DATA=$(cat <<EOF
 {
   "type": "s3",
@@ -98,23 +106,19 @@ create_snapshot_repository() {
 }
 EOF
 )
-  response=$(curl -s -u "$KIBANA_USERNAME:$KIBANA_PASSWORD" -X PUT "$ELASTICSEARCH_URL/_snapshot/$repository_name?pretty" -H 'Content-Type: application/json' -d "$JSON_DATA")
+  response=$(curl -s -u $KIBANA_CREDENTIALS -X PUT "$ELASTICSEARCH_URL/_snapshot/$REPO_NAME?pretty" -H 'Content-Type: application/json' -d "$JSON_DATA")
   if echo "$response" | grep -q "error"; then
-    # TODO: Change to exit_with_error
-    # exit_with_error "Failed to create repository, returned errors: $response"
-    echo "Failed to create repository, returned errors: $response"
+    exit_with_error "Failed to create repository, returned errors: $response"
   fi
   echo "Repository created successfully."
 }
 
 create_snapshot_policy() {
-  local repository_name="$CUSTOMER_NAME-$ENV_NAME-$REPO_NAME"
-  POLICY_NAME="$CUSTOMER_NAME-$ENV_NAME-snapshot-policy"
   JSON_DATA=$(cat <<EOF
 {
   "schedule": "0 30 1 * * ?",
   "name": "<daily-snap-{now/d}>",
-  "repository": "$repository_name",
+  "repository": "$REPO_NAME",
   "config": {
     "ignore_unavailable": false,
     "include_global_state": false
@@ -127,7 +131,7 @@ create_snapshot_policy() {
 }
 EOF
 )
-  response=$(curl -s -u "$KIBANA_USERNAME:$KIBANA_PASSWORD" -X PUT "$ELASTICSEARCH_URL/_slm/policy/$POLICY_NAME?pretty" -H 'Content-Type: application/json' -d "$JSON_DATA")
+  response=$(curl -s -u $KIBANA_CREDENTIALS -X PUT "$ELASTICSEARCH_URL/_slm/policy/$POLICY_NAME?pretty" -H 'Content-Type: application/json' -d "$JSON_DATA")
   if echo "$response" | grep -q "error"; then
     exit_with_error "Failed to create snapshot policy, returned errors: $response"
   fi
