402fe983cf1688462159ea53654d5be789f378fc
Suraj-Waghule <134491679+Suraj-Waghule@users.noreply.github.com>
2024-06-20 17:26:40 +0530

    Updated flyway script according to mysql-operator on k8's

--- deployment-scripts/flyway_automation/flyway_automation.sh
+++ deployment-scripts/flyway_automation/flyway_automation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Define variables
 db_name="testing_platform"
@@ -23,11 +23,11 @@ if command -v java &>/dev/null && [[ $(java -version 2>&1 | awk -F '"' '/version
   echo "Java 17 is already installed."
 else
   echo "Java 17 is not installed. Installing..."
-  
+
   # Install OpenJDK 17
-  sudo apt-get update
-  sudo apt-get install -y openjdk-17-jdk openjdk-17-jre
-  
+  apk update
+  apk add openjdk17
+
   # Verify installation
   if ! command -v java &>/dev/null || [[ $(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d '.' -f 1) != "17" ]]; then
     echo "Error: Java installation failed"
@@ -36,51 +36,35 @@ else
 fi
 
 # Maven Installation if required
-maven_install_dir="/opt/apache-maven-${maven_version}"
+if ! command -v mvn &>/dev/null; then
+    echo "Maven is not installed. Installing..."
 
-mvn_version=$(mvn -v | grep "Apache Maven ${maven_version}")
-if [ -z "$mvn_version" ]; then
-    echo "Maven ${maven_version} is not installed. Installing..."
-    maven_download_url="https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/${maven_version}/apache-maven-${maven_version}-bin.tar.gz"
-
-    # Download Maven
-    wget "$maven_download_url" -P /tmp || { echo "Error: Failed to download Maven"; exit 1; }
-
-    # Extract Maven
-    sudo tar xf /tmp/apache-maven-${maven_version}-bin.tar.gz -C /opt || { echo "Error: Failed to extract Maven"; exit 1; }
-
-    # Set Maven environment variables
-    echo 'export M2_HOME="${maven_install_dir}"' >> ~/.bashrc
-    echo 'export PATH=\"\$PATH:${maven_install_dir}/bin"' >> ~/.bashrc
-
-    # Set Maven environment variables
-    export M2_HOME="$maven_install_dir"
-    export PATH="$PATH:$M2_HOME/bin"
+    # Install Maven
+    apk add --no-cache maven="$maven_version"
 
     # Verify installation
-    mvn_version=$(mvn -v | grep "Apache Maven ${maven_version}")
-    if [ -z "$mvn_version" ]; then
+    if ! command -v mvn &>/dev/null; then
         echo "Error: Maven installation failed"
         exit 1
     fi
 fi
 
 # Install property-manager-java-services
-git clone https://$git_pat@github.com/whoosh-labs/property-manager-java-services.git
+git clone "https://$git_pat@github.com/whoosh-labs/property-manager-java-services.git"
 cd property-manager-java-services
 git checkout production
 mvn clean install -DskipTests
 cd ..
 
 # Install java-storage-library
-git clone https://$git_pat@github.com/whoosh-labs/java-storage-library.git
+git clone "https://$git_pat@github.com/whoosh-labs/java-storage-library.git"
 cd java-storage-library
 git checkout production
 mvn clean install -U
 cd ..
 
 # Install testing-platform-backend
-git clone https://$git_pat@github.com/whoosh-labs/testing-platform-backend.git
+git clone "https://$git_pat@github.com/whoosh-labs/testing-platform-backend.git"
 cd testing-platform-backend
 git checkout production
 mvn clean install -DskipTests
@@ -93,18 +77,18 @@ fi
 
 # Connect to MySQL and create database
 echo "Creating MySQL database '$db_name'..."
-sed -i -E 's/^(SET.*)/-- \1/' $sql_file
+sed -i -E 's/^(SET.*)/-- \1/' "$sql_file"
 mysql -u"$mysql_user" -p"$mysql_password" -h "$mysql_url" -e "CREATE DATABASE IF NOT EXISTS $db_name; USE $db_name; source $sql_file;" || { echo "Error: Failed to create database"; exit 1; }
 
 echo "Executing $sql_file in '$db_name' database..."
-mysql -u"$mysql_user" -p"$mysql_password" -h "$mysql_url" $db_name < "$sql_file" || { echo "Error: Failed to execute SQL file"; exit 1; }
+mysql -u"$mysql_user" -p"$mysql_password" -h "$mysql_url" "$db_name" < "$sql_file" || { echo "Error: Failed to execute SQL file"; exit 1; }
 
 # Write MySQL connection details to env file
 echo "MYSQL_USERNAME=${mysql_user}" >> "${envname}.db.ini"
 echo "MYSQL_PASSWORD=${mysql_password}" >> "${envname}.db.ini"
 echo "MYSQL_URL=${mysql_url}:3306" >> "${envname}.db.ini"
 
-bash flyway_migrate.sh --env ${envname}
+bash flyway_migrate.sh --env "${envname}"
 
 # Clearing directories
 cd ..
