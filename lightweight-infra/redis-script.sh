9564f599df0808e5218f6c18c748c1abb9a3a502
kanika-raga-ai <161692952+kanika-raga-ai@users.noreply.github.com>
2024-07-03 13:31:36 +0530

    Create redis-script.sh

---
+++ lightweight-infra/redis-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+
+helm install my-redis bitnami/redis
+
+
+echo "Waiting for Redis pods to be in running state..."
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=redis --timeout=300s
+
+
+helm status my-redis
+
+echo "Redis installation completed successfully."
